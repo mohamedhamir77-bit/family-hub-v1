0c22057cfc61c5a446c82f188d5dd0df137749b5
mohamedhamir77-bit <mohamed.hamir77@gmail.com>
2026-07-01 20:20:08 +0000

    V3 Step 4 - Improve add modal with selection state

--- dialogs.js
+++ dialogs.js
@@ -1,23 +1,29 @@
 const $ = id => document.getElementById(id);
 
 let selectedType = "folder";
-
+function updateSelectedType() {
+  document.querySelectorAll("[data-type]").forEach(button => {
+    button.classList.toggle("selected", button.dataset.type === selectedType);
+  });
+}
 export function showAddDialog() {
   return new Promise(resolve => {
     const modal = $("addModal");
     const nameInput = $("addItemName");
 
     selectedType = "folder";
+    updateSelectedType();
     nameInput.value = "";
     modal.classList.remove("hidden");
     nameInput.focus();
 
     document.querySelectorAll("[data-type]").forEach(button => {
-      button.onclick = () => {
-        selectedType = button.dataset.type;
-        nameInput.focus();
-      };
-    });
+  button.onclick = () => {
+    selectedType = button.dataset.type;
+    updateSelectedType();
+    nameInput.focus();
+  };
+});
 
     $("cancelAddModal").onclick = () => {
       modal.classList.add("hidden");
